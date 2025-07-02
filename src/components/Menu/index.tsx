fb2fdd0a9f7f508f67a826091da083034e1e926c
BADtochka <badmodertwitch@gmail.com>
2025-07-02 22:53:19 +0300

    chore: docs & examples

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -32,18 +32,18 @@ export default function Menu() {
       className='fixed bottom-[65px] left-[calc(50%-4px)] mx-1 h-[60px] w-[calc(100%-8px)] max-w-[504px] -translate-x-1/2 gap-2
         max-md:bottom-[20px]'
     >
-      <TabsContent customId='menu' className='rounded-full nth-[2]:w-full'>
+      <TabsContent customId='menu' className='rounded-full backdrop-blur-md nth-[2]:w-full'>
         <Button
           active={currentHash === 'main'}
           data-size='48px'
           iconSize='24px'
           iconLeft='logo'
-          className='z-10 size-[60px] bg-[#0a0a0a] p-[18px] mix-blend-difference data-[active="true"]:bg-black
+          className='z-10 size-[60px] bg-[#0a0a0a]/30 p-[18px] mix-blend-difference data-[active="true"]:bg-black/30
             data-[active="true"]:text-white'
           onClick={() => scrollToBlock('main')}
           animation={false}
         />
-        <div className='flex w-full items-center rounded-[64px] border border-[#ffffff28] bg-[#0a0a0a]'>
+        <div className='flex w-full items-center rounded-[64px] border border-[#ffffff28] bg-[#0a0a0a]/30'>
           <TabsContent customId='menu' className='w-full min-w-[42px] shrink'>
             {tabsKeys.map((tab, index) => {
               return (
@@ -56,7 +56,7 @@ export default function Menu() {
         </div>
         <Button
           iconLeft={clientLang === 'ru' ? 'ru' : 'en'}
-          className='size-[60px] bg-[#0a0a0a] p-[18px]'
+          className='size-[60px] bg-[#0a0a0a]/30 p-[18px]'
           iconSize='25px'
           onClick={toggleLanguage}
           animation={false}
